28a5d1c50743107845bf4ea0f16e7c2dd9b87543
ThanukaIshara <thanuka.17@business.mrt.ac.lk>
2020-10-10 12:52:19 +0530

    Added fixed point iterations python code

---
+++ Fixed point iteration method.py
@@ -0,0 +1,35 @@
+
+def fixedp(g,x0, tol, max_iteration):
+    e =1
+    itera = 0
+    xp = []
+    while (e > tol and itera <max_iteration):
+        
+        
+        x = g(x0)
+        
+        e = abs(x0-x) 
+        x0 = x
+        xp.append(x0) 
+        itr =+ 1
+        
+    print("Root of the equation = ", x)
+    print("Number of Iterations = ",len(xp) )
+
+def g(x):
+    g = (3*x + 2)**(1/2)
+    return g
+
+
+x0 =2
+tol = 0.001
+max_iteration = 100
+fixedp(g,x0, tol, max_iteration)
+
+
+
+
+
+
+
+
